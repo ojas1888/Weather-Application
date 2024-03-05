b2bb2ce48a87b79bda2ab069ba4da5872f0df9da
OJAS SRIVASTAVA <ojas.srivastava2003@gmail.com>
2024-03-05 18:29:02 +0530

    Update

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+    key: "03d28d5a4db0e7aae9b34a258bb28815",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
